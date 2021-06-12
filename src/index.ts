94489958b34a4189b4db7d241b094cf17059b2ef
Cindy Vo <cindyhvo824@gmail.com>
2021-06-12 17:01:29 -0400

    fixed no-console rule

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ const start = async () => {
         await app.ready();
         await app.listen(process.env.PORT || 3000);
     } catch (err) {
-        console.log(err);
         process.exit(1);
     }
 };
